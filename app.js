8c85746357031747dcd3255380e68cea324efbbe
AbdulHadiMeer <abdulhmeer@gmail.com>
2026-01-05 23:31:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const items = document.querySelectorAll('.accordion-item');
+
+items.forEach(item => {
+  item.querySelector('.accordion-header').addEventListener('click', () => {
+    items.forEach(i => {
+      i.classList.remove('active');
+      const btn = i.querySelector('.icon');
+      if (btn) btn.textContent = '+';
+    });
+
+    item.classList.add('active');
+    item.querySelector('.icon').textContent = '−';
+  });
+});
